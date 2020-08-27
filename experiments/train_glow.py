88c26de338251cc0b19b0bcf6f5f9cb31fcb53c6
Vincent Stimper <vincent.stimper@gmail.com>
2020-08-27 16:24:34 +0100

    Saving samples implemented

--- experiments/train_glow.py
+++ experiments/train_glow.py
@@ -4,12 +4,19 @@ import torchvision as tv
 import numpy as np
 import normflow as nf
 
+from matplotlib import pyplot as plt
+
 from . import utils
 from . import Glow
 
 import os
 import argparse
 
+from time import time
+
+
+# Log start time
+start_time = time()
 
 # Parse input arguments
 parser = argparse.ArgumentParser(description='Train Glow model on image dataset.')
@@ -18,8 +25,8 @@ parser.add_argument('--config', type=str, default='config/glow.yaml',
                     help='Path config file specifying model architecture and training procedure')
 parser.add_argument('--resume', action='store_true', help='Flag whether to resume training')
 parser.add_argument('--gpu', action='store_true', help='Flag whether to use gpu is available')
-#parser.add_argument('--tlimit', type=float, default=None,
-#                    help='Number of hours after which to stop training')
+parser.add_argument('--tlimit', type=float, default=None,
+                    help='Number of hours after which to stop training')
 
 args = parser.parse_args()
 
@@ -39,6 +46,7 @@ class_cond = config['model']['class_cond']
 # Load dataset
 if config['dataset']['name'] == 'cifar10':
     if class_cond:
+        num_classes = 10
         config['model']['num_classes'] = 10
 
     if config['dataset']['transform']['type'] == 'logit':
@@ -91,6 +99,7 @@ for dir in [cp_dir, sam_dir, log_dir]:
 max_iter = config['training']['max_iter']
 cp_iter = config['training']['cp_iter']
 log_iter = config['training']['log_iter']
+num_samples = config['training']['num_samples']
 
 loss_hist = np.zeros((0, 2))
 bpd_hist = np.zeros((0, 5))
@@ -136,6 +145,8 @@ for it in range(max_iter):
             b = nf.utils.bitsPerDim(model, x, y.to(device) if class_cond else None,
                                     trans=bpd_trans, trans_param=bpd_param)
             bpd_test = b.to('cpu').numpy()
+            del(x, y, b)
+            torch.cuda.empty_cache()
         bpd_append = np.array([[it + 1, np.nanmean(bpd_train), np.nanstd(bpd_train),
                                 np.nanmean(bpd_test), np.nanstd(bpd_test)]])
         bpd_hist = np.concatenate([bpd_hist, bpd_append])
@@ -143,3 +154,26 @@ for it in range(max_iter):
                    header='it,train_mean,train_std,test_mean,test_std', comments='')
         np.savetxt(os.path.join(cp_dir, 'loss.csv'), loss_hist, delimiter=',',
                    header='it,loss', comments='')
+
+    if (it + 1) % cp_iter:
+        # Save checkpoint
+        model.save(os.path.join(cp_dir, 'model_%07i.pt' % (it + 1)))
+        torch.save(optimizer.state_dict(), os.path.join(cp_dir, 'optimizer.pt'))
+
+        # Generate samples
+        with torch.no_grad():
+            if class_cond:
+                y = torch.arange(num_classes).repeat(num_samples).to(device)
+                nrow = num_classes
+            else:
+                y = None
+                nrow = 8
+            x, _ = model.sample(num_samples, y=y)
+            if config['dataset']['transform']['type'] == 'logit':
+                x = logit.inverse(x)
+            x_ = torch.clamp(x.cpu(), 0, 1)
+            img = np.transpose(tv.utils.make_grid(x_, nrow=nrow).numpy(), (1, 2, 0))
+            plt.imsave(os.path.join(sam_dir, 'samples_%07i.png' % (it + 1)), img)
+
+    if args.tlimit is not None and (time() - start_time) / 3600 > args.tlimit:
+        break
