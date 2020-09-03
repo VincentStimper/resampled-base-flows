cb95da0e891c6da93583a3b7191515a4b9c37178
Vincent Stimper <vincent.stimper@gmail.com>
2020-09-03 17:05:16 +0100

    One data loader worker for debugging

--- experiments/train_glow.py
+++ experiments/train_glow.py
@@ -84,7 +84,7 @@ if config['dataset']['name'] == 'cifar10':
     train_data = tv.datasets.CIFAR10(config['dataset']['path'], train=True, download=True,
                                      transform=tv.transforms.Compose(train_trans))
     train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
-                                               shuffle=True, num_workers=4, pin_memory=True)
+                                               shuffle=True)#, num_workers=4, pin_memory=True)
 
     test_data = tv.datasets.CIFAR10(config['dataset']['path'], train=False, download=True,
                                     transform=tv.transforms.Compose(test_trans))
