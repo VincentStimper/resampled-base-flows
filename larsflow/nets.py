06e1e6f7068bd6b4a04f0d97ce4b9f4cf45d0aaf
Vincent Stimper <vincent.stimper@gmail.com>
2020-09-25 16:30:11 +0100

    Added output function

--- larsflow/nets.py
+++ larsflow/nets.py
@@ -2,19 +2,24 @@ import torch
 from torch import nn
 from . import utils
 
+import normflow as nf
+
 class ConvNet2d(nn.Module):
     """
     Convolutional Neural Network with leaky ReLU nonlinearities and a
     final fully connected output layer
     """
 
-    def __init__(self, channels, kernel_size, output_units, leaky=0.0):
+    def __init__(self, channels, kernel_size, output_units, leaky=0.0,
+                 output_fn="sigmoid"):
         """
         Constructor
         :param channels: List of channels of conv layers, first entry is in_channels
         :param kernel_size: List of kernel sizes, same for height and width
         :param output_units: List of two ints
         :param leaky: Leaky part of ReLU
+        :param output_fn: String, function to be applied to the output, either
+        None, "sigmoid", or "clampexp"
         """
         super().__init__()
         # Build network
@@ -25,6 +30,10 @@ class ConvNet2d(nn.Module):
             net.append(nn.LeakyReLU(leaky))
         net.append(nn.Flatten())
         net.append(nn.Linear(*output_units))
+        if output_fn == "sigmoid":
+            net.append(nn.Sigmoid())
+        elif output_fn == "clampexp":
+            net.append(nf.utils.ClampExp())
         self.net = nn.Sequential(*net)
 
     def forward(self, x):
