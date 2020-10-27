2c771064d94a4e02edfa45f71090b5aa88d11f6d
Vincent Stimper <vincent.stimper@gmail.com>
2020-10-27 10:12:22 +0000

    First draft of Boltzmann generator model with config

--- larsflow/core.py
+++ larsflow/core.py
@@ -1,6 +1,13 @@
 import torch
 import numpy as np
 import normflow as nf
+import boltzgen as bg
+
+from simtk import openmm as mm
+from simtk import unit
+from simtk.openmm import app
+from openmmtools import testsystems
+import mdtraj
 
 from . import distributions
 
@@ -145,3 +152,159 @@ class Glow(nf.MultiscaleFlow):
                 return out
         else:
             return -self.log_prob(x, y)
+
+
+class BoltzmannGenerator(NormalizingFlow):
+    """
+    Boltzmann Generator with architecture inspired by arXiv:2002.06707
+    """
+    def __init__(self, config):
+        """
+        Constructor
+        :param config: Dict, specified by a yaml file, see sample config file
+        """
+
+        self.config = config
+        # Set up simulation object
+        if config['system']['name'] == 'AlanineDipeptideVacuum':
+            ndim = 66
+            z_matrix = [
+                (0, [1, 4, 6]),
+                (1, [4, 6, 8]),
+                (2, [1, 4, 0]),
+                (3, [1, 4, 0]),
+                (4, [6, 8, 14]),
+                (5, [4, 6, 8]),
+                (7, [6, 8, 4]),
+                (11, [10, 8, 6]),
+                (12, [10, 8, 11]),
+                (13, [10, 8, 11]),
+                (15, [14, 8, 16]),
+                (16, [14, 8, 6]),
+                (17, [16, 14, 15]),
+                (18, [16, 14, 8]),
+                (19, [18, 16, 14]),
+                (20, [18, 16, 19]),
+                (21, [18, 16, 19])
+            ]
+            cart_indices = [6, 8, 9, 10, 14]
+            temperature = config['system']['temperature']
+
+            if config['system']['constraints']:
+                self.system = testsystems.AlanineDipeptideVacuum()
+            else:
+                self.system = testsystems.AlanineDipeptideVacuum(constraints=None)
+            if config['system']['platform'] == 'CPU':
+                self.sim = app.Simulation(self.system.topology, self.system.system,
+                                          mm.LangevinIntegrator(temperature * unit.kelvin,
+                                                                1. / unit.picosecond,
+                                                                1. * unit.femtosecond),
+                                          mm.Platform.getPlatformByName('CPU'))
+            elif config['system']['platform'] == 'Reference':
+                self.sim = app.Simulation(self.system.topology, self.system.system,
+                                          mm.LangevinIntegrator(temperature * unit.kelvin,
+                                                                1. / unit.picosecond,
+                                                                1. * unit.femtosecond),
+                                          mm.Platform.getPlatformByName('Reference'))
+            else:
+                self.sim = app.Simulation(self.system.topology, self.system.system,
+                                          mm.LangevinIntegrator(temperature * unit.kelvin,
+                                                                1. / unit.picosecond,
+                                                                1. * unit.femtosecond),
+                                          mm.Platform.getPlatformByName(config['system']['platform']),
+                                          {'Precision': config['system']['precision']})
+
+            training_data = torch.randn(66, 66)
+        else:
+            raise NotImplementedError('The system ' + config['system']['name']
+                                      + ' has not been implemented.')
+
+        # Load data for transform if specified
+        if config['data_path'] is not None:
+            # Load the alanine dipeptide trajectory
+            traj = mdtraj.load(config['data_path'])
+            traj.center_coordinates()
+
+            # superpose on the backbone
+            ind = traj.top.select("backbone")
+
+            traj.superpose(traj, 0, atom_indices=ind, ref_atom_indices=ind)
+
+            # Gather the training data into a pytorch Tensor with the right shape
+            training_data = traj.xyz
+            n_atoms = training_data.shape[1]
+            n_dim = n_atoms * 3
+            training_data_npy = training_data.reshape(-1, n_dim)
+            training_data = torch.from_numpy(training_data_npy.astype("float64"))
+
+        # Set up model
+        # Define flows
+        blocks = config['model']['blocks']
+
+        # Set target distribution
+        energy_cut = config['system']['energy_cut']
+        energy_max = config['system']['energy_max']
+        add_transform = config['model']['transform']
+        transform = bg.flows.CoordinateTransform(training_data, ndim,
+                                                 z_matrix, cart_indices)
+
+        if 'parallel_energy' in config['system'] and config['system']['parallel_energy']:
+            if add_transform:
+                p = bg.distributions.BoltzmannParallel(self.system, temperature,
+                        energy_cut=energy_cut, energy_max=energy_max,
+                        n_threads=config['system']['n_threads'])
+            else:
+                p = bg.distributions.TransformedBoltzmannParallel(self.system, temperature,
+                        energy_cut=energy_cut, energy_max=energy_max, transform=transform,
+                        n_threads=config['system']['n_threads'])
+        else:
+            if add_transform:
+                p = bg.distributions.Boltzmann(self.sim.context, temperature,
+                        energy_cut=energy_cut, energy_max=energy_max)
+            else:
+                p = bg.distributions.TransformedBoltzmann(self.sim.context, temperature,
+                        energy_cut=energy_cut, energy_max=energy_max, transform=transform)
+
+        # Set up parameters for flow layers
+        hidden_units = config['model']['hidden_units']
+        hidden_layers = config['model']['hidden_layers']
+        scale = True if config['model']['coupling'] == 'affine' else False
+        init_zeros = config['model']['init_zeros']
+
+        # Set up base distribution
+        latent_size = config['model']['latent_size']
+        if config['model']['base']['type'] == 'resampled':
+            T = config['model']['base']['params']['T']
+            eps = config['model']['base']['params']['eps']
+            a_hl = config['model']['base']['params']['a_hidden_layers']
+            a_hu = config['model']['base']['params']['a_hidden_units']
+            a = nf.nets.MLP([latent_size] + a_hl * [a_hu] + [1], output_fn="sigmoid")
+            q0 = nf.distributions.ResampledGaussian(latent_size, a, T, eps,
+                                trainable=config['model']['base']['learn_mean_var'])
+        else:
+            q0 = nf.distributions.DiagGaussian(latent_size,
+                                trainable=config['model']['base']['learn_mean_var'])
+
+        # Set up flow layers
+        flows = []
+        for i in range(blocks):
+            # Coupling layer
+            param_map = nf.nets.MLP([latent_size] + hidden_layers * [hidden_units]
+                                    + [latent_size * (2 if scale else 1)], init_zeros=init_zeros)
+            flows += [nf.flows.AffineCouplingBlock(param_map, scale=scale)]
+
+            # Permutation
+            if config['model']['permutation'] == 'affine':
+                flows += [nf.flows.InvertibleAffine(latent_size)]
+            else:
+                flows += [nf.flows.Permute(latent_size, config['model']['permutation'])]
+
+            # ActNorm
+            if config['model']['actnorm']:
+                flows += [nf.flows.ActNorm(latent_size)]
+        # Coordinate transformation
+        if add_transform:
+            flows += [transform]
+
+        # Construct flow model
+        super().__init__(q0=q0, flows=flows, p=p)
