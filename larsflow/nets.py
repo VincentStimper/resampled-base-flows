e77f5004687cb07478007ea712cee947e579b3ed
Vincent Stimper <vincent.stimper@gmail.com>
2020-10-31 17:15:04 +0000

    Stride added to net

--- larsflow/nets.py
+++ larsflow/nets.py
@@ -10,23 +10,31 @@ class ConvNet2d(nn.Module):
     final fully connected output layer
     """
 
-    def __init__(self, channels, kernel_size, output_units, leaky=0.0,
-                 output_fn=None):
+    def __init__(self, channels, output_units, kernel_size=3, stride=1,
+                 leaky=0.0, output_fn=None):
         """
         Constructor
         :param channels: List of channels of conv layers, first entry is in_channels
-        :param kernel_size: List of kernel sizes, same for height and width
+        :param kernel_size: Int of list of ints, same for height and width, if int
+        same kernel size for each layer is chosen
         :param output_units: List of two ints
+        :param stride: Int or list of int, if int same stride for all layers is used
         :param leaky: Leaky part of ReLU
         :param output_fn: String, function to be applied to the output, either
         None, "sigmoid", or "clampexp"
         """
         super().__init__()
+        # Prepare parameters
+        n_layers = len(channels) - 1
+        if isinstance(stride, int):
+            stride = n_layers * [stride]
+        if isinstance(kernel_size, int):
+            kernel_size = n_layers * [kernel_size]
         # Build network
         net = nn.ModuleList([])
-        for i in range(len(kernel_size)):
+        for i in range(n_layers):
             net.append(nn.Conv2d(channels[i], channels[i + 1], kernel_size[i],
-                                 padding=kernel_size[i] // 2))
+                                 padding=kernel_size[i] // 2), stride=stride[i])
             net.append(nn.LeakyReLU(leaky))
         net.append(nn.Flatten())
         net.append(nn.Linear(*output_units))
