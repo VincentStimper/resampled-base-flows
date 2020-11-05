3cfe9484b5f3b5a88c7d9d318addf63cc8031b30
Vincent Stimper <vincent.stimper@gmail.com>
2020-11-05 17:29:25 +0000

    Fixed iter creation

--- experiments/train_boltzmann_generator.py
+++ experiments/train_boltzmann_generator.py
@@ -148,7 +148,7 @@ batch_size = config['training']['batch_size']
 train_loader = torch.utils.data.DataLoader(training_data, batch_size=batch_size,
                                            shuffle=True, pin_memory=True,
                                            drop_last=True, num_workers=8)
-train_iter = iter(training_data)
+train_iter = iter(train_loader)
 
 
 # Start training
