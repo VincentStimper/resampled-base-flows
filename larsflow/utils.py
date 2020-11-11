9db326329721018c67526b2798ddd3498081858d
Vincent Stimper <vincent.stimper@gmail.com>
2020-11-11 14:50:40 +0000

    Remove superfluous plot

--- larsflow/utils.py
+++ larsflow/utils.py
@@ -234,6 +234,7 @@ def evaluateAldp(model, test_data, n_samples=1000, n_batches=100,
                 fig, ax = plt.subplots(3, 3, figsize=(10, 10))
             else:
                 fig, ax = plt.subplots(6, 3, figsize=(10, 20))
+                ax[5, 2].set_axis_off()
             for j in range(hists_train_list[i].shape[1]):
                 ax[j // 3, j % 3].plot(x, hists_train_list[i][:, j])
                 ax[j // 3, j % 3].plot(x, hists_gen_list[i][:, j])
